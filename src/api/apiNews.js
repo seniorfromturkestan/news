7f7cd8cfb1c2d026b7279fcba4f1e651dfc8f8f0
seniorfromturkestan <yerassyltay2004@mail.ru>
2024-11-23 15:18:22 +0500

    MobileMenu добавил

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -5,16 +5,3 @@ export const getNews = async () => {
 
   return response;
 };
-
-export const getNewsById = async (newsId) => {
-  const response = await axiosInstance.get(`latest-news/${newsId}`);
-
-  return response;
-};
-
-export const createNews = async (newsBody) => {
-  const response = await axiosInstance.post(`news`, newsBody);
-
-  return response;
-}
-
--- src/pages/Company/Company.jsx
+++ src/pages/Company/Company.jsx
@@ -1,12 +1,10 @@
-import React, { useState } from 'react'
-import { User } from '../../components/User/User'
 
 const Company = () => {
   
 
   return (
     <div>
-     
+        Hello
     </div>
   )
 }
